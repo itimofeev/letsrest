b7edc47dacedcb588a7053dd545fb1a2cb38dbaa
Ilya Timofeev <ilya.ufo@gmail.com>
2017-04-05 08:01:37 +0300

    * added to deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#rsync -a /Users/ilyatimofee/prog/js/hustlesa-ui/sync/ ilyaufo@188.166.26.165:/home/ilyaufo/vhustle/frontend/
 scp -r tools/target/* ilyaufo@188.166.26.165:/home/ilyaufo/letsrest
